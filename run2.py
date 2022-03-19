eeb85fe20b08c8d3b5df9ce391912cb9d6c911e7
Kacper015 <kacpermyczewski@gmail.com>
2022-03-19 11:32:18 +0000

    Added code to check if there will be place to place down a ship' clear

--- run2.py
+++ run2.py
@@ -70,3 +70,24 @@ def print_grid():
     for i in range(len(grid[0])):
         print(str(i), end=" ")
     print("")
+
+
+def validate_grid_and_place_ship(start_row, end_row, start_col, end_col):
+    """
+    This will check the row or column to see if able to place ship 
+    """
+    global grid
+    global ship_positions
+
+    all_valid = True
+    for r in range(start_row, end_row):
+        for c in range(start_col, end_col):
+            if grid[r][c] != ".":
+                all_valid = False
+                break
+    if all_valid:
+        ship_positions.append([start_row, end_row, start_col, end_col])
+        for r in range(start_row, end_row):
+            for c in range(start_col, end_col):
+                grid[r][c] = "O"
+    return all_valid
